2aaa41eacac25083627eeaf28050d740d4fd5bde
K-Henry <dev@machinename.dev>
2024-12-09 23:51:53 -0600

    Name Change

--- server.ts
+++ server.ts
@@ -11,9 +11,11 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 
-// CORS configuration to allow requests from the login domain
 const corsOptions = {
     origin: [
+        'https://account.machinename.dev',
+        'https://idea.machinename.dev',
+        'https://login.machinename.dev',
         'https://machinename.dev',
         'https://www.machinename.dev',
     ],
@@ -25,19 +27,57 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
-// Default route
+app.use((req: Request, res: Response, next: Function) => {
+    console.log(`Request method: ${req.method}, URL: ${req.url}`);
+    next();
+});
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('<h1>Machine Name Services</h1>');
+    res.send('<h1>Machine Name Auth</h1>');
+});
+
+app.post('/login', async (req: Request, res: Response): Promise<void> => {
+    const idToken = req.body.idToken;
+
+    if (!idToken) {
+        res.status(400).json({ message: 'ID Token Required' });
+        return;
+    }
+
+    try {
+        const verifiedIdToken = await admin.auth().verifyIdToken(idToken);
+        const customToken = await admin.auth().createCustomToken(verifiedIdToken.uid);
+
+        res.cookie('MNFBCT', customToken, {
+            domain: '.machinename.dev',
+            maxAge: 60 * 60 * 1000, 
+            httpOnly: true,
+            secure: true, 
+            sameSite: 'none',
+        });
+
+        res.status(200).json({ message: 'Custom Token Created'});
+    } catch (error) {
+        console.error('Error Verifying ID Token:', error);
+        const errorMessage = (error as Error).message;
+        res.status(401).json({ message: 'Unauthorized', error: errorMessage });
+    }
 });
 
 const server = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server Is Running On Port ${port}`);
 });
 
-// Handle graceful shutdown of server
 process.on('SIGTERM', () => {
-    console.log('SIGTERM signal received. Closing HTTP server.');
+    console.log('SIGTERM Signal Received. Closing HTTP Server.');
     server.close(() => {
-        console.log('HTTP server closed.');
+        console.log('HTTP Server Closed.');
+    });
+});
+
+process.on('SIGINT', () => {
+    console.log('SIGINT Signal Received. Closing HTTP Server.');
+    server.close(() => {
+        console.log('HTTP Server Closed.');
     });
 });
